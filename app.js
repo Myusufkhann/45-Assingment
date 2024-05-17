d28806a83ed6d264ac50f77680a0cebb9472e739
Myusufkhann <160284864+Myusufkhann@users.noreply.github.com>
2024-05-17 13:02:13 -0700

    Assignment # 10
    10.	Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.

---
+++ app.js
@@ -0,0 +1,5 @@
+//Assingment no 10
+//my name is M Yusuf Khan
+//Date: 13/04/2024
+//This program will run simple code just like Hellow Yusuf
+console.log('Hellow Yusuf simple program');
---
+++ program2.ts
@@ -0,0 +1,5 @@
+//Assingment no 10
+//my name is M Yusuf Khan
+//Date: 13/04/2024
+//This program will Multiplication
+console.log(3 * 2);
